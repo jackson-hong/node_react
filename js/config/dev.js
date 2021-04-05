a3b0ac79550636997361cbf6ac0f6e4df714c79c
Jackson Hong <jacksonhong0111@gmail.com>
2021-04-05 20:49:16 +0900

    second

---
+++ js/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://jackson:ghdwldns21@cluster0.bcvxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- js/index.js
+++ js/index.js
@@ -4,12 +4,14 @@ const port = 5000
 const bodyParser = require('body-parser')
 const { User }= require('./models/User')
 
+const config = require('./config/key')
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 
 
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://jackson:ghdwldns21@cluster0.bcvxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log("MongoDB connected..."))
     .catch(err => console.log(err))
